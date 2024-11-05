f253067694b7aee1428221cbefc9243b8e711529
Karim <karimm.fadli@gmail.Com>
2024-11-05 16:07:36 +0100

    Add DI Module

---
+++ app/src/main/java/com/voodoo/adtech/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.voodoo.adtech.di
+
+import com.voodoo.adtech.ui.screen.HomeViewModel
+import com.voodoo.sdk.SDKVoodoo
+import org.koin.core.module.dsl.*
+import org.koin.dsl.module
+import kotlin.math.sin
+
+val appModule = module {
+
+    single { SDKVoodoo() }
+    viewModel { HomeViewModel(get()) }
+
+}
